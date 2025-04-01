2762b741c8e9bcbb7afca8c5b201936f5bbb1262
ppauline25 <pauline.prak@gmail.com>
2025-04-01 22:18:13 +0200

    modif pauline

--- Script/ActionGroupes-Pauline.sh
+++ Script/ActionGroupes-Pauline.sh
@@ -4,3 +4,13 @@
 # version : 1.0 
 # Description : script projet 2 
 ######################################################################
+echo "fdgvd"
+
+case "expression" in
+    pattern)
+        ddvfv ...
+        ;;
+    *)
+        command ...
+        ;;
+esac
--- Script/InformationsUser-Pauline.sh
+++ Script/InformationsUser-Pauline.sh
@@ -2,19 +2,22 @@
 #! /bin/bash
 # Auteur : Pauline 
 # version : 1.0 
-# Description : script Informations User ######################################################################
+# Description : script Informations User 
+######################################################################
 
 echo "merci de bien vouloir spécifier l'utilisateur concerné" 
 read user 
-
+# verification si user fait parti de la liste des utilisateurs de l'ordinateur 
+# [ $user -e $echo(who) ]
 echo "1 => Date dernière connexion de l'utilisateur " 
 echo "2 => Date dernière modification du mot de passe de l'utilisateur " 
 echo "3 => Liste des sessions ouvertes par l'utilisateur "  
 echo "x => Quitter "  
 read choix
 
-case choix in
-    1) # Date de dernière connexion d'un user 
+case $choix in
+    1) # Date de dernière connexion d'user 
+    # format date à retravailler
         last -n1 $user
         ;;
     2) # Date dernière modification mdp d'un user 
