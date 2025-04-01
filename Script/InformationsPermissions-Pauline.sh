da725cf5b8f6ea0773d07c69cbf79966028e1dc8
ppauline25 <pauline.prak@gmail.com>
2025-04-01 17:16:42 +0200

    Update InformationsPermissions-Pauline.sh

--- Script/InformationsPermissions-Pauline.sh
+++ Script/InformationsPermissions-Pauline.sh
@@ -4,3 +4,45 @@
 # version : 1.0 
 # Description : script projet 2 
 ######################################################################
+
+
+ajouter un droit 
+  Pour utilisateur ?
+      Ajouter R?
+      Ajouter W?
+      Ajouter X?
+      Retour ? 
+      
+  pour groupe ? 
+      Ajouter R?
+      Ajouter W?
+      Ajouter X? 
+      Retour ? 
+      
+  pour autre ? 
+      Ajouter R?
+      Ajouter W?
+      Ajouter X?   
+      Retour ? 
+
+supprimer un droit ? 
+  pour utilisateur ?
+      Supprimer R?
+      Supprimer W?
+      Supprimer X? 
+      Retour ?
+      
+  pour groupe ? 
+      Supprimer R?
+      Supprimer W?
+      Supprimer X? 
+      Retour ? 
+      
+  pour autre ? 
+      Supprimer R?
+      Supprimer W?
+      Supprimer X?   
+      Retour ?
+
+Retour au menu ?
+
