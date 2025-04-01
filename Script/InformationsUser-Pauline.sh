7285490160ecaf9d91905e685eaccb9b46b035c7
ppauline25 <pauline.prak@gmail.com>
2025-04-01 22:07:54 +0200

    modif pauline

--- Script/InformationsUser-Pauline.sh
+++ Script/InformationsUser-Pauline.sh
@@ -2,51 +2,26 @@
 #! /bin/bash
 # Auteur : Pauline 
 # version : 1.0 
-# Description : script Informations User 
-######################################################################
-$1=user
-# vérification de $1 
-    if [ $# -eq 1 ]; 
-    then
-        echo "1 => Date dernière connexion de l'utilisateur " choix
-        echo "2 => Date dernière modification du mot de passe de l'utilisateur " choix
-        echo "3 => Liste des sessions ouvertes par l'utilisateur " choix 
-        echo "x => Quitter " choix 
-        read $choix
+# Description : script Informations User ######################################################################
 
-            case choix in ( liste )
-                # Date de dernière connexion d’un utilisateur : 
-                1)  if [ $# -eq 1 ]; 
-                    then
-                        last -n1 $user ; 
-                    else 
-                        echo "Veuillez ajouter un nom d'utilisateur en paramètre" ; 
-                    fi 
-                # Date dernière modification mot de passe d'un utilisateur : 
-                2)  if [ $# -eq 1 ]; 
-                    then
-                        chage -l $user | awk 'NR==1' ; 
-                    else 
-                    echo "Veuillez ajouter un nom d'utilisateur en paramètre" ; 
-                    fi 
-                # Liste des sessions ouvertes par un utilisateur : 
-                3)
-                # Revenir au menu précédent/Quitter le script : 
-                x)exit 
-            esac
+echo "merci de bien vouloir spécifier l'utilisateur concerné" 
+read user 
 
-    else 
-        echo "Veuillez renseigner un nom d"utilisateur en paramètre"
-        exit 
+echo "1 => Date dernière connexion de l'utilisateur " 
+echo "2 => Date dernière modification du mot de passe de l'utilisateur " 
+echo "3 => Liste des sessions ouvertes par l'utilisateur "  
+echo "x => Quitter "  
+read choix
 
-    fi
-
-
-
-
-
-
-# Liste des sessions ouvertes par l'utilisateur ??????
-who
-w 
-users 
+case choix in
+    1) # Date de dernière connexion d'un user 
+        last -n1 $user
+        ;;
+    2) # Date dernière modification mdp d'un user 
+        chage -l $user | awk 'NR==1'
+        ;;
+    3) # Liste des sessions ouvertes par l'utilisateur 
+        # commande à chercher 
+        ;;
+    
+esac
