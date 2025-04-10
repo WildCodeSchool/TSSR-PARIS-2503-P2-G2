d2909ba263a268b83b68f947f8d2fdbbef7f3bf4
ppauline25 <pauline.prak@gmail.com>
2025-04-10 19:39:04 +0200

    Update Informations_OS.sh

--- Scripts Bash/Informations_OS.sh
+++ Scripts Bash/Informations_OS.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 # Auteur : Pauline 
 # version : 1.0 
-# Description : script projet 2 
+# Description : script informations OS projet 2 
 ######################################################################
 
 
