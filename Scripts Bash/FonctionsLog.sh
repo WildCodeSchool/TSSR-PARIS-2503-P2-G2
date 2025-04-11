7ecf3871e641709aacf4b67a91d44b4124495276
ppauline25 <pauline.prak@gmail.com>
2025-04-11 22:39:06 +0200

    modifs

--- Scripts Bash/FonctionsLog.sh
+++ Scripts Bash/FonctionsLog.sh
@@ -6,58 +6,48 @@
 # A valider + ajouter dans le script global 
 ######################################################################
 
-#fonction à ajouter au début du script global 
+# Fonction à ajouter au début du script global
 function INITIALISATION ()
 {
-#Initialisation des variables pour le fichier log 
-ORDINATEUR=$(hostname)
-DATE=$(date +"%d-%m-%Y")
-HEURE=$(date +"%H:%M:%S")
-FICHIERLOG="Info_${ORDINATEUR}_${DATE}.log"
-LOGPATH="/var/log/$FICHIERLOG"
+    # Initialisation des variables pour le fichier log 
+    ORDINATEUR=$(hostname)
+    DATE=$(date +"%d-%m-%Y")
+    HEURE=$(date +"%H:%M:%S")
+    FICHIERLOG="Info_${ORDINATEUR}_${DATE}.log"
+    LOGPATH="/var/log/$FICHIERLOG"
 
-#Création fichier log 
-sudo touch "$FICHIERLOG"
-#Ajout ==========StartScript========== dans le fichier log 
-echo "$DATE$HEURE$ORDINATEUR==========StartScript==========" >> $FICHIERLOG
+    # Création du fichier log
+    sudo touch "$LOGPATH"
+    
+    # Ajout ==========StartScript========== dans le fichier log
+    echo "$DATE $HEURE $ORDINATEUR ==========StartScript==========" | sudo tee -a "$LOGPATH" > /dev/null
 
-#changements droits fichier log
-sudo chmod 600 "$FICHIERLOG"
+    # Changements des droits sur le fichier log
+    sudo chmod 600 "$LOGPATH"
 }
 
-
-
-
-
-#fonction à ajouter à chaque début de code 
+# Fonction à ajouter à chaque début de code 
 function ENTREE ()
 {
-echo "$0" | sudo tee -a /var/log/"$FICHIERLOG" > /dev/null
-echo "$1" | sudo tee -a /var/log/"$FICHIERLOG" > /dev/null
+    echo "$0" | sudo tee -a "$LOGPATH" > /dev/null
+    echo "$1" | sudo tee -a "$LOGPATH" > /dev/null
 }
 
-
-
-
-
-#fonction à ajouter à chaque fin de code 
+# Fonction à ajouter à chaque fin de code 
 function SORTIE ()
 {
-if #? [ -eq 0 ]; 
-then 
-echo "Ceci est une sortie standard" >> /var/log/"$FICHIERLOG" 2>&1
-else  
-echo "Ceci est une sortie d'erreur" >> /var/log/"$FICHIERLOG" 2>&1
-fi 
+    if [ $? -eq 0 ]; then
+        # Si le code de retour est 0 (succès), afficher "Ceci est une sortie standard"
+        echo "Ceci est une sortie standard" | sudo tee -a "$LOGPATH" > /dev/null
+    else  
+        # Si le code de retour est différent de 0 (échec), afficher "Ceci est une sortie d'erreur"
+        echo "Ceci est une sortie d'erreur" | sudo tee -a "$LOGPATH" > /dev/null
+    fi 
 }
 
-
-
-
-
-#fonction à ajouter avant la sortie du script global 
+# Fonction à ajouter avant la sortie du script global 
 function FIN_SCRIPT ()
 {
-#Ajout ==========EndScript========== dans le fichier log 
-echo "$DATE$HEURE$ORDINATEUR==========EndScript==========" >> $FICHIERLOG
+    # Ajout ==========EndScript========== dans le fichier log
+    echo "$DATE $HEURE $ORDINATEUR ==========EndScript==========" | sudo tee -a "$LOGPATH" > /dev/null
 }
