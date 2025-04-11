5d3369686878cd444a8e73a6fdfbba7d0a683349
ppauline25 <pauline.prak@gmail.com>
2025-04-11 10:54:49 +0200

    Update Informations_Materiels.sh

--- Scripts Bash/Informations_Materiels.sh
+++ Scripts Bash/Informations_Materiels.sh
@@ -3,6 +3,7 @@
 # Auteur : Chahine MARZOUK
 # Version : 1.1
 # Description : Script Informations Système - exécuté à distance via SSH
+# OK SSH - option 1 bug
 ######################################################################
 
 # Demander l'adresse IP ou le nom d'hôte de la machine distante
