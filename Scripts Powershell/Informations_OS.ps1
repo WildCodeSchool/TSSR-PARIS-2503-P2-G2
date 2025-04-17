0238739905cd6edafd87a369e5a52ec59124444d
ppauline25 <pauline.prak@gmail.com>
2025-04-17 10:33:14 +0200

    aaa

--- Scripts Powershell/Informations_OS.ps1
+++
@@ -1,9 +0,0 @@
-####################################################################################
-# Autor : Pauline 
-# Version : 1.0 
-# Description : information os.ps1
-# Etat : 
-####################################################################################
-
-Get-ComputerInfo | Select-Object WindowsProductName, WindowsVersion, OsBuildNumber
-#Commande retour menu précédent 
