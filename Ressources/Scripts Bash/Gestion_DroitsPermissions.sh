148bcada77d990a26c2c24af5dc76b55a5e1c615
ppauline25 <pauline.prak@gmail.com>
2025-04-22 19:52:38 +0200

    Update Gestion_DroitsPermissions.sh

--- Ressources/Scripts Bash/Gestion_DroitsPermissions.sh
+++ Ressources/Scripts Bash/Gestion_DroitsPermissions.sh
@@ -7,15 +7,20 @@
 
 # Infos SSH
 read -p "Adresse IP de la machine distante : " ip
+echo "$DATE-$HEURE-$USER-$1 : demande IP de la machine distante : $ip" >> $LOGFILE
+
 read -p "Nom d'utilisateur SSH : " ssh_user
+echo "$DATE-$HEURE-$USER-$1 : demande nom utilisateur de la machine distante : $ssh_user" >> $LOGFILE
 
 # Fichier/dossier et utilisateur cible
 read -p "Veuillez indiquer le chemin du fichier/dossier : " chemin 
+echo "$DATE-$HEURE-$USER-$1 : demande path du fichier/dossier : $chemin" >> $LOGFILE
+
 read -p "Veuillez indiquer l'utilisateur concerné : " user 
+echo "$DATE-$HEURE-$USER-$1 : demande nom utilisateur concerne : $user" >> $LOGFILE
 
 # Mot de passe sudo (caché à l'affichage)
 read -s -p "Mot de passe sudo du user distant : " sudo_pass
-echo
 
 # Vérification du fichier et de l'utilisateur sur la machine distante
 
@@ -36,6 +41,8 @@ echo "→ pour ajouter un droit : add "
 echo "→ pour supprimer un droit : del "
 echo "→ pour retourner dans le menu précédent : retour "
 read -p "Votre choix : " choix
+echo "$DATE-$HEURE-$USER-$1 : réponse de l'utilisateur $choix" >> $LOGFILE
+
 
 while [ "$choix" != "retour" ]; do
 
@@ -46,6 +53,8 @@ case "$choix" in
                 echo "→ pour ajouter "execution" : +x "
                 echo "→ pour retourner au menu précédent : retour "
                 read -p "Votre choix : " choix_add
+                echo "$DATE-$HEURE-$USER-$1 : choix droit $choix_add" >> $LOGFILE
+
 
             while [ "$choix_add" != "retour" ]; do
             case "choix" in
@@ -75,6 +84,8 @@ case "$choix" in
                 echo "→ pour supprimer "execution" : -x "
                 echo "→ pour retourner au menu précédent : retour "
                 read -p "Votre choix : " choix_del
+                echo "$DATE-$HEURE-$USER-$1 : choix utilisateur $choix_del" >> $LOGFILE
+
                 
             while [ "$choix_del" != "retour" ]; do
             case "$choix" in
