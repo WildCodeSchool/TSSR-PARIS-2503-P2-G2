b93c4a7f8f6ab1ace261bd4f824bc7e0876ffeb5
ChahineM <chahine38@hotmail.fr>
2025-04-22 17:02:22 +0200

    Update Informations_Materiels.ps1

--- Scripts Powershell/Informations_Materiels.ps1
+++ Scripts Powershell/Informations_Materiels.ps1
@@ -1,19 +1,8 @@
-######################################################################
-# Auteur : Chahine MARZOUK 
-# Version : 2.0 (WinRM)
-# Description : Script d'informations système via PowerShell/WinRM
-######################################################################
-
-# Forcer l'encodage UTF-8 pour l'affichage correct
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-# Configuration de la cible
 $client = Read-Host "Nom ou IP de la machine distante"
-
-# Authentification
 $creds = Get-Credential -Message "Entrez les identifiants pour la machine distante $client"
 
-# Fonction de menu
 function Show-Menu {
     Clear-Host
     Write-Host "=== Informations Système sur ($client) ==="
@@ -35,23 +24,20 @@ do {
                 Get-CimInstance Win32_Processor | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors
             }
         }
-
         "2" {
             Invoke-Command -ComputerName $client -Credential $creds -ScriptBlock {
                 $mem = Get-CimInstance Win32_ComputerSystem
-                Write-Output ("Mémoire RAM Totale : {0} Go" -f [math]::Round($mem.TotalPhysicalMemory / 1GB, 2))
+                "Mémoire RAM Totale : {0} Go" -f [math]::Round($mem.TotalPhysicalMemory / 1GB, 2)
             }
         }
-
         "3" {
             Invoke-Command -ComputerName $client -Credential $creds -ScriptBlock {
                 Get-CimInstance Win32_OperatingSystem | ForEach-Object {
-                    Write-Output ("RAM utilisée : {0} Go" -f [math]::Round(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)/1MB, 2))
-                    Write-Output ("RAM disponible : {0} Go" -f [math]::Round($_.FreePhysicalMemory/1MB, 2))
+                    "RAM utilisée : {0} Go" -f [math]::Round(($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)/1MB, 2)
+                    "RAM disponible : {0} Go" -f [math]::Round($_.FreePhysicalMemory/1MB, 2)
                 }
             }
         }
-
         "4" {
             Invoke-Command -ComputerName $client -Credential $creds -ScriptBlock {
                 Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3" | Select-Object `
@@ -60,19 +46,16 @@ do {
                     @{Name='Espace libre (Go)';Expression={[math]::Round($_.FreeSpace/1GB,2)}}
             }
         }
-
         "5" {
             Invoke-Command -ComputerName $client -Credential $creds -ScriptBlock {
                 $cpuLoad = Get-CimInstance Win32_Processor | Select-Object -ExpandProperty LoadPercentage
-                Write-Output ("Utilisation CPU : $cpuLoad %")
+                "Utilisation CPU : $cpuLoad %"
             }
         }
-
         "6" {
             Write-Host "Fin du script."
             break
         }
-
         default {
             Write-Host "Option invalide. Veuillez choisir un nombre entre 1 et 6."
         }
