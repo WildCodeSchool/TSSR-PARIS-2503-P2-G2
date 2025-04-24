572358d917a19a1d6adc4986c2233bd08169eb79
ppauline25 <pauline.prak@gmail.com>
2025-04-24 09:26:03 +0200

    Update Gestion_Groupes.sh

--- Scripts/Library/Gestion_Groupes.sh
+++ Scripts/Library/Gestion_Groupes.sh
@@ -6,13 +6,13 @@
 ######################################################################
 
 read -p "Adresse IP de la machine distante : " ip
-echo "$DATE-$HEURE-$USER-$1 : adresse ip communiqué : $ip" >> $LOGFILE
+sudo echo "$DATE-$HEURE-$USER-$1 : adresse ip communiqué : $ip" >> $LOGFILE
 
 read -p "Nom d'utilisateur SSH : " ssh_user
-echo "$DATE-$HEURE-$USER-$1 : utilisateur distant communiqué $ssh_user" >> $LOGFILE
+sudo echo "$DATE-$HEURE-$USER-$1 : utilisateur distant communiqué $ssh_user" >> $LOGFILE
 
 read -p "Nom de l'utilisateur à modifier : " user
-echo "$DATE-$HEURE-$USER-$1 : utilisateur a modifier $user" >> $LOGFILE
+sudo echo "$DATE-$HEURE-$USER-$1 : utilisateur a modifier $user" >> $LOGFILE
 
 
 echo
@@ -23,34 +23,34 @@ echo "Pour supprimer d'un groupe local  : 3"
 echo "Pour quitter                      : retour"
 echo "------------------------------------------------------------"
 read -p "Choix : " choix
-echo "$DATE-$HEURE-$USER-$1 : choix utilisateur $choix" >> $LOGFILE
+sudo echo "$DATE-$HEURE-$USER-$1 : choix utilisateur $choix" >> $LOGFILE
 
 
 while [ "$choix" != "retour" ]; do
     case "$choix" in
         1) 
         # Ajouter au groupe admin
-            echo "$DATE-$HEURE-$USER-$1 : choix utilisateur : ajouter $user au groupe admin" >> $LOGFILE
+            sudo echo "$DATE-$HEURE-$USER-$1 : choix utilisateur : ajouter $user au groupe admin" >> $LOGFILE
             ssh -t "${ssh_user}@${ip}" << EOF
             sudo usermod -aG adm "$user"
-            echo "$DATE-$HEURE-$USER-$1 : $user ajouté au groupe admin" >> $LOGFILE
+            sudo echo "$DATE-$HEURE-$USER-$1 : $user ajouté au groupe admin" >> $LOGFILE
             echo "$user ajouté au groupe adm"
 EOF
         ;;
         2) 
         # Ajouter au groupe local
-            echo "$DATE-$HEURE-$USER-$1 : choix utilisateur : ajouter au groupe local" >> $LOGFILE
+            sudo echo "$DATE-$HEURE-$USER-$1 : choix utilisateur : ajouter au groupe local" >> $LOGFILE
         	cat /etc/group
             read -p "Groupe local dans lequel ajouter $user : " group
             ssh -t "${ssh_user}@${ip}" << EOF 
             if grep -q "^$group:" /etc/group; then  
                     sudo usermod -aG "$group" "$user"
                     echo "$user ajouté au groupe $group"  
-                    echo "$DATE-$HEURE-$USER-$1 : $user ajouté au groupe $group" >> $LOGFILE
+                    sudo echo "$DATE-$HEURE-$USER-$1 : $user ajouté au groupe $group" >> $LOGFILE
 
             else 
                     echo "Groupe $group inexistant"
-                    echo "$DATE-$HEURE-$USER-$1 : $group inexistant" >> $LOGFILE
+                    sudo echo "$DATE-$HEURE-$USER-$1 : $group inexistant" >> $LOGFILE
 
             fi 
 EOF
@@ -59,21 +59,21 @@ EOF
         # Suppression du groupe local
         	cat /etc/group
             read -p "Groupe local dans lequel supprimer $user : " group
-            echo "$DATE-$HEURE-$USER-$1 : choix utilisateur : suppression $user du groupe local" >> $LOGFILE
+            sudo echo "$DATE-$HEURE-$USER-$1 : choix utilisateur : suppression $user du groupe local" >> $LOGFILE
             ssh -t "${ssh_user}@${ip}" << EOF
             if grep -q "^$group:" /etc/group; then
                     sudo gpasswd -d "$user" "$group"
-                    echo "$DATE-$HEURE-$USER-$1 : $user supprimé du groupe $group" >> $LOGFILE
+                    sudo echo "$DATE-$HEURE-$USER-$1 : $user supprimé du groupe $group" >> $LOGFILE
                     echo "$user supprimé du groupe $group"
             else
-                    echo "$DATE-$HEURE-$USER-$1 : Groupe $group inexistant" >> $LOGFILE
+                    sudo echo "$DATE-$HEURE-$USER-$1 : Groupe $group inexistant" >> $LOGFILE
                     echo "Groupe $group inexistant"
             fi 
 EOF
         ;;
         retour)
             echo "Retour menu précedent. "
-            echo "$DATE-$HEURE-$USER-$1 : retour au menu precedent" >> $LOGFILE
+            sudo echo "$DATE-$HEURE-$USER-$1 : retour au menu precedent" >> $LOGFILE
             sleep 3
         ;;
     esac
